6041aadd895395653c8b98e315e07efee8ddf0c1
Marco <marco.amador.roman@gmail.com>
2024-03-23 00:19:51 -0600

    Incorporar stream de datos

--- src/main/java/com/alpha/servicio/repositorios/IAutomovilRepository.java
+++ src/main/java/com/alpha/servicio/repositorios/IAutomovilRepository.java
@@ -1,13 +1,15 @@
 package com.alpha.servicio.repositorios;
-
-import java.util.ArrayList;
+import java.util.stream.Stream;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.alpha.servicio.modelos.AutomovilModelo;
 
+import jakarta.transaction.Transactional;
+
 @Repository
+@Transactional
 public interface IAutomovilRepository extends JpaRepository<AutomovilModelo, Long> {
-    public ArrayList<AutomovilModelo> findByIdCliente_Club_IdClubAndIdCliente_ActivoAndActivo(Long idClub, int clienteActivo, int automovilActivo);
+    public Stream<AutomovilModelo> findByIdCliente_Club_IdClubAndIdCliente_ActivoAndActivo(Long idClub, int clienteActivo, int automovilActivo);
 }
--- src/main/java/com/alpha/servicio/servicios/AutomovilServicio.java
+++ src/main/java/com/alpha/servicio/servicios/AutomovilServicio.java
@@ -1,6 +1,7 @@
 package com.alpha.servicio.servicios;
 
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,17 +9,20 @@ import org.springframework.stereotype.Service;
 import com.alpha.servicio.modelos.AutomovilModelo;
 import com.alpha.servicio.repositorios.IAutomovilRepository;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class AutomovilServicio {
     @Autowired
     IAutomovilRepository automovilRepository;
 
+    @Transactional
     public ArrayList<Long> buscarChips(Long idClub){
-        ArrayList<AutomovilModelo> automovilesActivos = automovilRepository.findByIdCliente_Club_IdClubAndIdCliente_ActivoAndActivo(idClub, 1, 1);
+        Stream<AutomovilModelo> automovilesActivos = automovilRepository.findByIdCliente_Club_IdClubAndIdCliente_ActivoAndActivo(idClub, 1, 1);
         ArrayList<Long> chipsFinales = new ArrayList<Long>();
-        for (AutomovilModelo automovilModelo : automovilesActivos) {
-            chipsFinales.add(automovilModelo.getIdChip());
-        }
+        automovilesActivos.forEach((automovil)->{
+            chipsFinales.add(automovil.getIdChip());
+        });
         return chipsFinales;
     }
 }
